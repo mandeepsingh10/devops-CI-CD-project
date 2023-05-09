8035a43f469fc153db3908bc2ad70ed4a3f7172c
mandeepsingh10 <mandeepsingh1018@gmail.com>
2023-05-09 22:58:40 +0530

    Updated config files

--- terraform_config/nexus/instance.tf
+++ terraform_config/nexus/instance.tf
@@ -4,6 +4,10 @@ resource "aws_instance" "web" {
   instance_type          = var.instance_type
   key_name               = aws_key_pair.terra-key.key_name
   vpc_security_group_ids = ["${aws_security_group.allow_req_ports_nexus.id}"]
+  ebs_block_device {
+    device_name = "/dev/sda1"
+    volume_size = 20
+  }
   user_data = file("script.sh")
   tags = {
     Name = var.aws_instance_name
